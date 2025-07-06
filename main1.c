174ab0ddd3438c2a6c9b37e302e4aa5edd6c220d
ashlee <ashleina20@gmail.com>
2025-07-07 00:42:25 +0800

    Update main1.c
    javadoc

--- main1.c
+++ main1.c
@@ -1,6 +1,22 @@
+/**
+ * CCDSALG MCO1 GROUP 23
+ * Description: a program that computes the convex hull of 2D points using Graham’s Scan with selectionSort.
+ * Authors:
+ * Laborada, Nathan
+ * Lee, Ashley Fiona
+ * Villorente, Khyle Raeke
+ */
+
 #include "driver-helper.h"
 #include "graham-helper.h"
- 
+
+/**
+ * This is the main function for running the convex hull program.
+ * It asks the user for filenames, reads the points from a file,
+ * then calls grahamScan1(), which uses selectionSort, to compute the convex hull.
+ *
+ * @return 0 if it runs successfully, 1 otherwise
+ */
 int main() {
     Filename inputFilename, outputFilename; 
     Point points[MAX_STACK_SIZE];
@@ -8,11 +24,13 @@ int main() {
     int n;
     int i;
 
+    // Ask user for input and output filenames
     printf("Enter the filename to import points from: ");
     scanf("%s", inputFilename);
     printf("Enter the filename to export points to: ");
     scanf("%s", outputFilename);
     
+    // Read points from the input file
     importInput(inputFilename, points, &n);
 
     if (n <= 0) {
@@ -21,14 +39,17 @@ int main() {
     }
     printf("Successfully read %d points from %s.\n", n, inputFilename);
 
+    // Compute convex hull using Graham's Scan (selection sort)
     grahamScan1(points, n, &hull);
 
+    // Display convex hull points
     printf("Convex hull points:\n");
     for (i = 0; i < hull.top + 1; i++)
     {
         printf("Point %d: (%f, %f)\n", i + 1, hull.data[i].x, hull.data[i].y);
     }
 
+    // Export convex hull points to the output file
     exportInput(outputFilename, hull.data, hull.top + 1);
 
     printf("Convex hull computation completed successfully.\n");
